083f57748f0769e827bc66c8648f318f2c06c9d6
HubertWdowiak <hubert.wdowiak98@onet.pl>
2023-01-15 00:55:26 +0100

    removed gh action job

--- test_app.py
+++ test_app.py
@@ -1,17 +1,17 @@
 from unittest import TestCase
 from unittest.mock import patch, Mock
 
-from . import create_app, get_db_connection
+from setup import create_app, get_db_connection
 
 
 class Test(TestCase):
 
-    @patch("src.Flask")
+    @patch("setup.Flask")
     def test_create_app(self, flask_mock: Mock):
         create_app()
         flask_mock.assert_called_once()
 
-    @patch("src.create_engine")
+    @patch("setup.create_engine")
     def test_get_db_connection(self, create_engine_mock: Mock):
         get_db_connection()
         create_engine_mock.assert_called_once()
